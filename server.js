ccc839ec6ccd01c80cc974c9712c197e4e67ecf1
Greg Abdo <gpabdo@gmail.com>
2020-11-05 19:37:16 -0800

    feat: add ping and subscribe

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const WS_PORT = process.env.WS_PORT || 8080;
 const REDIS_HOST = process.env.REDIS_HOST || "redis";
 const REDIS_PORT = process.env.REDIS_HOST || 6379;
 
+
+const client = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
+client.pubsub('channels', (err, channels) => {
+  if (err) {
+    console.log( err )
+  } else {
+    console.log('Channels:', channels); 
+  }
+});
+client.quit();
+
+
 // Web Socket
 const wss = new WebSocket.Server({ port: WS_PORT });
 
@@ -15,22 +27,54 @@ wss.on('connection', function connection(ws, req) {
 
   // New ws connection, lets connect to redis.
   const subscriber = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
+  const publisher = redis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
   
   // Received a message.
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    subscriber.subscribe("telemetry");    
+   
+    // Make sure it's json
+    obj = {} 
+    try {
+      obj = JSON.parse(message);
+    }
+    catch(e) {
+      console.log( "Message error: " + e )
+      return
+    }
+
+    // We are looking for a type and data
+    if( !obj.type || !obj.data )
+      return
+
+    // Do something with the message
+    switch (obj.type) {
+      case 'subscribe':
+        obj.data.forEach( function( channel ) {
+           subscriber.subscribe( channel );
+        });
+        break;
+      case 'ping':
+        ws.send( JSON.stringify( {"type":"pong", "data": { "time": obj.data }}) );
+        break;
+      case 'command':
+        publisher.publish( "commands", obj.data );
+        break;
+ 
+      default:
+        console.log("Huh?: " + obj.type)
+      }
+
   });
   
   // New message from redis Pub/Sub.
   subscriber.on("message", function(channel, message) {
-    ws.send(message);
+    ws.send( message );
   })
 
   // Error with redis.
   subscriber.on("error", function(error) {
     console.log( error )
-    ws.send( '{"redis": "error", "data": error}' );
+    ws.send( '{"type": "redis", "data": {"error": error}}' );
   })
 
   // ws client has disconnected.
